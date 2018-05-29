30b191013b6a9fd41f97d55dfd494b78a5ba88e3
HONG ZHOU <holly_zhou@yahoo.com>
2018-05-29 12:39:32 -0700

    add actions

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import MovieActions from '../actions/MovieActions';
 
 
 export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieKeyword: ""
+    };
+    this.searchMovie = this.searchMovie.bind(this);
+  }
+
+  searchMovie() {
+    let {movieKeyword} = this.state;
+    MovieActions.searchOneMovie(movieKeyword);
+  }
+
   render() {
     return (
       <div className="container">
         <h1 className="text-center">Movie App</h1>
-        
+
       </div>
     )
   }
