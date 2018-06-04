da59e84d0959a2e11972a1925ccd589644500e9a
HONG ZHOU <holly_zhou@yahoo.com>
2018-06-03 22:16:29 -0700

    delete route

--- server/models/movieInfo.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-exports.getMovieInformation = (query, cb) => {
-  axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API}&t=${movieKeyWords}`)
-    .then(res => cb(null, res.data))
-    .catch(process.stderr.write);
-};
